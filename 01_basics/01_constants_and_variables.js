ec0222ded45c0576c7227cf18ada2ce071754b27
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-05-16 19:30:59 +0000

    Day 1

---
+++ 01_basics/01_constants_and_variables.js
@@ -0,0 +1,36 @@
+// Declare a constant variable for account ID.
+const accountId = 1001;
+
+// Declare a let variable for account email
+let accountEmail = "connect@codeartisanriz.dev";
+
+// Declare a var variable for account password
+var accountPassword = "12345";
+
+// Declare a global variable for account city (no keyword used)
+accountCity = "Jaipur";
+
+// Declare a let variable for account state, but do not initialize it.
+let accountState;
+
+// The following line is commented out because it would cause an error as Constants cannot be reassigned.
+// accountId = 2 // not allowed.
+
+// Update the email address.
+accountEmail = "hc@hc.com";
+
+// Update the password.
+accountPassword = "21212121";
+
+// Update the city.
+accountCity = "Bengaluru";
+
+// Log the account ID to the console.
+console.log(accountId);
+
+/*
+Prefer not to use var because of issues in block scope and functional scope.
+*/
+
+// Display account details in a table format.
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
