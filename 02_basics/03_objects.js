faef60f48d042382d7683783a899afbb8825bdc9
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-05-23 23:17:26 +0530

    Object Basics

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,41 +1,44 @@
-// Singleton 
-Object.create
+// Singleton and Object.create
 
 // symbol
-const mySym = Symbol("key1")
+const mySym = Symbol("key1") // Creating a symbol for unique property keys
 
 // Object Literals
-
 const jsUser = {
     name: "Rizwan",
     age: 27,
-    "full name": "Rizwan Mazumder",
-    [mySym]: "mykey1", // defining symbol. need to use [] brackets
+    "full name": "Rizwan Mazumder", // Using spaces in property names requires quotes
+    [mySym]: "mykey1", // Defining a symbol property, using brackets for dynamic keys
     location: "Silchar",
     email: "connect@codeartisan.dev",
     isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+    lastLoginDays: ["Monday", "Saturday"] // Array property
 }
-// keys are by default treated as strings
+// Keys in objects are by default treated as strings
 
-console.log(jsUser.email) // commonly used (. notation)
-console.log(jsUser["email"]) // best practise
-// console.log(jsUser.full name) // cannot call "full name" via . method, "" are also not allowed in . method
-console.log(jsUser["full name"]) // can use 
-console.log(jsUser[mySym]) // logging mysym Symbol
+console.log(jsUser.email) // Commonly used dot notation for accessing properties
+console.log(jsUser["email"]) // Bracket notation, useful for dynamic or non-standard keys
+// console.log(jsUser.full name) // Cannot call "full name" via dot notation
+console.log(jsUser["full name"]) // Bracket notation is required for keys with spaces
+console.log(jsUser[mySym]) // Accessing the symbol property
 
-jsUser.email = "hello@codeartisan.dev" // change values
-// Object.freeze(jsUser) // freezes/locks the object
-jsUser.email = "yo@codeartisan.dev" // wont work as ojbect is locked, also wont give any error.
-console.log(jsUser["email"])
+// Changing property values
+jsUser.email = "hello@codeartisan.dev" // Updating the email property
+// Object.freeze(jsUser) // Freezes the object(jsUser), making it immutable. 
+// Note: Once an object has been frozen there is no way to unfreeze it.
+jsUser.email = "yo@codeartisan.dev" // This change won't work if the object is frozen
+console.log(jsUser["email"]) // Logs the updated email
 
-console.log(jsUser)
+console.log(jsUser) // Logs the entire jsUser object
 
+// Adding methods to the object
 jsUser.greeting = function(){
     console.log("Hello js User")
 }
-console.log(jsUser.greeting())
+console.log(jsUser.greeting()) // Calls the greeting method
+
+// Adding another method that references object properties
 jsUser.greetingTwo = function(){
-    console.log(`Hello js User: ${this.name}`) // referencing name from object. here this refers to jsUser
+    console.log(`Hello js User: ${this.name}`) // 'this' refers to the jsUser object
 }
-console.log(jsUser.greetingTwo())
+console.log(jsUser.greetingTwo()) // Calls the greetingTwo method
