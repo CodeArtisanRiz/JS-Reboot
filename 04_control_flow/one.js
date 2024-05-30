5ff10396f6a65eac0122ecedfe25da49a71ab66b
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-05-30 18:16:29 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,9 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+if ( temperature === 40 ){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
