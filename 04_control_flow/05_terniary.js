e9d147c8aab90c6d0ed901462d49176e1c4930d6
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-06-07 12:55:48 +0000

    terniary

---
+++ 04_control_flow/05_terniary.js
@@ -0,0 +1,14 @@
+// Terniary Operator
+
+// condition ? expressionIfTrue : expressionIfFalse;
+
+let age = 20;
+let allowed = age >= 18 ? 'Yes' : 'No';
+
+console.log(allowed); // 'Yes'
+
+
+let speed = 70;
+let message = speed > 60 ? 'Too Fast' : (speed < 30 ? 'Too Slow' : 'Optimal');
+
+console.log(message); // 'Too Fast'
