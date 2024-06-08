e20dce8a9c1d1f3dad14d2ec16d559d794f69dd2
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-06-08 09:23:31 +0000

    for()

--- 05_loops_iterations/01_for_break_continue.js
+++ 05_loops_iterations/01_for_break_continue.js
@@ -1,36 +1,36 @@
-// For
+// For loop examples
 
+// Basic for loop to iterate from 0 to 9
 for (let i = 0; i < 10; i++) {
     const element = i;
     if(element == 5){
-        // console.log("Halfway Crossed")
+        console.log("Halfway Crossed") // Prints when halfway through the loop
     }
-    // console.log(element);
-    
+
+    console.log(element) // Prints each element
 }
-// console.log(element); // Err
 
+// console.log(element); // Error: Will cause an error because 'element' is not defined outside the loop
 
+// Nested for loops
 for (let i = 1; i <= 10; i++) {
-    // console.log(`Outer loop value: ${i}`);
+    console.log(`Outer loop value: ${i}`); // Prints the value of the outer loop
     for (let j = 1; j <= 10; j++) {
-        // console.log(`Inner loop value: ${j} and inner loop: ${i}`);
-        // console.log(i + '*' + j + ' = ' + i*j);
+        console.log(`Inner loop value: ${j} and outer loop: ${i}`); // Prints the value of the inner loop and the product of i and j
+        console.log(i + '*' + j + ' = ' + i*j); // Prints tables of 1-10
     }
 }
 
 let myArr = ["Flash", "Batman", "Superman"]
-// console.log(myArr.length)
+// console.log(myArr.length) // Prints the length of the array
 for (let index = 0; index < myArr.length; index++) {
     const element = myArr[index];
-    // console.log(element)
-    
+    console.log(element) // Prints each element of the array
 }
 
-// Keywords: 
-// break and continue
+// Keywords: break and continue
 
-
+// Example of using 'break' to exit the loop when index is 5
 for (let index = 1; index <= 20; index++) {
     if(index == 5 ){
         console.log(`Got ${index}`);
@@ -39,10 +39,11 @@ for (let index = 1; index <= 20; index++) {
     console.log(`Value of i is: ${index}`);
 }
 
+// Example of using 'continue' to skip the current iteration when index is 5
 for (let index = 1; index <= 20; index++) {
     if(index == 5 ){
         console.log(`Got ${index}`);
-        continue // Skip current
+        continue // Skip current iteration
     }
     console.log(`Value of i is: ${index}`);
 }
