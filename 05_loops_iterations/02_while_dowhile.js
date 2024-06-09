30514984588c3df8868fa43fb5040b89f03fca3e
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-06-09 09:16:28 +0000

    while() & dowhile()

--- 05_loops_iterations/02_while_dowhile.js
+++ 05_loops_iterations/02_while_dowhile.js
@@ -1,25 +1,25 @@
-// While
-// while (condition) {
-    
-// }
+// While Loop
 
+// Basic while loop to increment index by 2 until it's greater than 10
 let index = 0
 while (index <= 10) {
-    console.log(`Value of index os ${index}`)
-    index = index + 2
+    console.log(`Value of index is ${index}`)
+    index += 2 // Increment index by 2
 }
 
+// Iterating over an array using a while loop
 let myArr = ["Flash", "Batman", "Superman"]
 let arr = 0
 while (arr < myArr.length) {
     console.log(`Value of arr is ${myArr[arr]}`)
-    arr = arr + 1
+    arr++ // Increment arr to move to the next element
 }
 
-
+// Do-While Loop
 let score = 11
 
+// This loop will execute at least once even if the condition is false initially
 do {
     console.log(`Score is ${score}`)
-    score++
-} while (score <= 10);
+    score++ // Increment score
+} while (score <= 10); // Condition checked after the block is executed
