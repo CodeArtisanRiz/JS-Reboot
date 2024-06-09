b36624560c041b7ac45d008106c611b9d62e478d
H M Rizwan Mazumder <42119262+CodeArtisanRiz@users.noreply.github.com>
2024-06-09 09:15:00 +0000

    while & dowhile

---
+++ 05_loops_iterations/02_while_dowhile.js
@@ -0,0 +1,25 @@
+// While
+// while (condition) {
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index os ${index}`)
+    index = index + 2
+}
+
+let myArr = ["Flash", "Batman", "Superman"]
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`Value of arr is ${myArr[arr]}`)
+    arr = arr + 1
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
